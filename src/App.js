3963982cad49da9acf9c32949e3af31fc315b318
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-07-18 10:42:16 -0500

    say hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+// Imported component from react library
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    // Created constructor to create state of this class
+    constructor() {
+        super();
+        this.state = {
+            name: {
+                firstname: 'miguel',
+                lastname: 'romero'
+            },
+        };
+    }
+
+    // Built in react function to render html
+    render() {
+        return (
+            <div className="App"></div>
+        );
+    }
+
 }
 
 export default App;
